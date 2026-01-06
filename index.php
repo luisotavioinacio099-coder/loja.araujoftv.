c06bb919ea2af380968075ded569206cb7e4ee27
luisotavioinacio099-coder <luis.otavio.inacio099@gmail.com>
2026-01-06 14:44:21 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,271 @@
+
+<!DOCTYPE html>
+<html lang="pr-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARAUJO | FTV</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- NAVBAR -->
+    <div class="navbar">
+        <div class="header-inner-content">
+            <h1 class="logo">ARAUJO <span> FUTVOLEI</span></h1>
+
+            <nav>
+                <ul id="MenuItens">
+                    <li><a href="loja_virtual.html">Início</a></li>
+                    <li><a href="produtos.html">Produtos</a></li>
+                    <li><a href="empresa.html">Empresa</a></li>
+                    <li><a href="contato.html">Contatos</a></li>
+                    <li><a href="minha-conta.html">Minha Conta</a></li>
+                </ul>
+            </nav>
+
+            <div class="nav-icon-conteiner">
+                <img src="images/logo 5.jpeg">
+                <img src="images/bg branco.jpeg" class="menu-button">
+            </div>
+        </div>
+    </div>
+
+    <!-- HEADER -->
+    <header>
+        <div class="header-inner-content">
+            <div class="header-buttom-side">
+                <div class="header-buttom-side-left">
+                    <h2>Deia um Novo Estilo ao seu Trabalho!</h2>
+                    <p>
+                        Sucesso nem sempre tem haver com grandeza. Tem haver com
+                        consistência. Trabalho duro e consistência superam tudo!
+                    </p>
+                    <button> Ver Agora &#8594</button>
+                </div>
+
+                <div class="header-buttom-side-right">
+                    <img src="images/bg branco.jpeg">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- CONTEÚDO PRINCIPAL -->
+    <main>
+
+        <!-- LOGOS -->
+        <div class="gray-background">
+            <div class="page-inner-content">
+                <div class="cols cols-3">
+                    <img src="images/logo 1.jpeg">
+                    <img src="images/logo 2.jpeg">
+                    <img src="images/logo 4.jpeg">
+                </div>
+            </div>
+        </div>
+
+        <!-- PRODUTOS SELECIONADOS -->
+        <div class="page-inner-content">
+            <h3 class="section-title">Produtos Selecionado</h3>
+            <div class="subtitle-underline"></div>
+
+            <a href="produto_1.html" title="">
+                <div class="cols cols-4">
+                    <div class="product">
+                        <img src="images/bg branco.jpeg" id="produto_1">
+                        <p class="product-name">Kit Big Wolf Branco</p>
+                        <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                        <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+
+                    </div>
+            </a>
+
+            <a href="produto_2.html" title="">
+                <div class="product">
+                    <img src="images/bg preto.jpeg" id="produto_2">
+                    <p class="product-name">Kit Big Wolf Preto</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+            </a>
+
+            <a href="produto_3.html" title="">
+                <div class="product">
+                    <img src="images/Fminino.jpeg" id="produto_3">
+                    <p class="product-name">Kit Araujo Ftv Feminino</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+            </a>
+
+            <a href="produto_4.html" title="">
+                <div class="product">
+                    <img src="images/Garrfa.jpeg">
+                    <p class="product-name">Garrafa Térmica Araujo FTV</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+            </a>
+
+        </div>
+        </div>
+
+        <!-- ÚLTIMOS PRODUTOS -->
+        <div class="page-inner-content">
+            <h3 class="section-title">Últimos Produtos</h3>
+            <div class="subtitle-underline"></div>
+
+            <div class="cols cols-4">
+
+                <div class="product">
+                    <img src="images/bola.jpeg">
+                    <p class="product-name">Bolas Mikasa</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/bg branco.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/bg preto.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/Fminino.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/Garrfa.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/bola.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/bg branco.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+                <div class="product">
+                    <img src="images/bg preto.jpeg">
+                    <p class="product-name">Adicionar nome</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p class="product-price">$ 0,000.00 <span>REAIS</span></p>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- BLOCO EXCLUSIVO -->
+        <div class="gray-background">
+            <div class="header-inner-content">
+                <div class="header-buttom-side exclusive-container">
+                    <div class="header-buttom-side-left">
+                        <h2>Deia um Novo Estilo ao seu Trabalho!</h2>
+                        <p>
+                            Sucesso nem sempre tem haver com grandeza. Tem haver com
+                            consistência. Trabalho duro leva ao sucesso!
+                        </p>
+                        <button>Ver Agora &#8594;</button>
+                    </div>
+
+                    <div class="header-buttom-side-right">
+                        <img src="images/bg branco.jpeg">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- DEPOIMENTOS -->
+        <div class="page-inner-content">
+            <div class="testimonials">
+
+                <div class="testimonial">
+                    <p>"</p>
+                    <p>O melhor uniforme para seus jogos</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p>Programador Luis Otavio</p>
+                </div>
+
+                <div class="testimonial">
+                    <p>"</p>
+                    <p>O melhor uniforme para seus jogos</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p>Programador Luis Otavio</p>
+                </div>
+
+                <div class="testimonial">
+                    <p>"</p>
+                    <p>O melhor uniforme para seus jogos</p>
+                    <p class="rate">&#9733; &#9733; &#9733; &#9733; &#9734;</p>
+                    <p>Programador Luis Otavio</p>
+                </div>
+
+            </div>
+        </div>
+
+    </main>
+
+    <!-- FOOTER -->
+    <footer class="gray-background">
+        <div class="page-inner-content footer-content">
+
+            <div class="download-options">
+                <p>Conheça nosso Instagram !</p>
+
+                <div>
+
+                    <img src="images/Instagram_logo_2022.svg.png">
+                </div>
+            </div>
+
+            <div class="logo-footer">
+                <h1 class="logo">ARAUJO <span> FUTVOLEI</span></h1>
+                <p>Nosso objetivo é melhorar seu estilo no FTV.</p>
+            </div>
+
+        </div>
+
+        <hr class="page-inner-content">
+
+        <div class="page-inner-content copyright">
+            <p>copyright 2025 - Luis Otavio - Todos os Direitos Reservados</p>
+        </div>
+
+    </footer>
+
+    <script>
+        const navbar = document.querySelector(".navbar");
+        const menuButton = document.querySelector(".menu-button");
+
+        menuButton.addEventListener('click', () => {
+            navbar.classList.toggle("show-menu");
+        });
+    </script>
+
+</body>
+
+</html>
